154201a6f1edb830467003f1eb1de60e0e6b8833
livium669 <mrliviu76@gmail.com>
2020-09-02 21:15:16 +0200

    airbnb-clone

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>2020 Airbnb clone.</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+        </div>
+    )
+}
+
+export default Footer;
